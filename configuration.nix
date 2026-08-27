42e3220394376bb2312cf91d42809849b419e808
BandiFee <bandifee@gmail.com>
2026-08-28 02:05:54 +0800

    feat: customize tuigreet with a sci-fi login theme

--- configuration.nix
+++ configuration.nix
@@ -158,6 +158,43 @@
   # Login
   # ============================================================
 
+  environment.etc."tuigreet/config.toml".text = ''
+    [display]
+    show_time = true
+    time_format = "[ %Y-%m-%d || %H:%M ]"
+    greeting = "[+] Welcome back to NixOS on ThinkPad T14 [+]"
+    align_greeting = "center"
+
+    [layout]
+    width = 48
+    window_padding = 1
+    container_padding = 1
+    prompt_padding = 1
+
+    [layout.widgets]
+    time_position = "top"
+    status_position = "hidden"
+
+    [remember]
+    username = true
+    session = false
+    user_session = true
+
+    [secret]
+    mode = "characters"
+    characters = "*"
+
+    [theme]
+    border = "green"
+    text = "white"
+    time = "green"
+    container = "black"
+    title = "green"
+    greet = "green"
+    prompt = "white"
+    input = "green"
+  '';
+
   services.greetd = {
     enable = true;
 
@@ -166,7 +203,7 @@
 
     settings.default_session = {
       command =
-        "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd ${config.programs.niri.package}/bin/niri-session";
+        "${pkgs.tuigreet}/bin/tuigreet --config /etc/tuigreet/config.toml --cmd ${config.programs.niri.package}/bin/niri-session";
 
       user = "greeter";
     };
